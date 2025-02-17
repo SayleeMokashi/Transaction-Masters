7e19ca9f26d942d9bcd977eb51c4bd3c6873e8d8
SayleeMokashi <sayleev@gmail.com>
2025-02-17 21:47:41 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,8 +16,7 @@ function Navbar() {
 
   <div class="nav-right d-flex align-items-center ms-auto">
     <div class="contact-info me-4">
-      <p class="m-0">Contact Us:</p>
-      <p class="m-0">Phone: 248-716-4247</p>
+      <p class="m-1">Phone: 248-716-4247</p>
       <p class="m-0">Email: <a href="mailto:team@transaction-masters.com">team@transaction-masters.com</a></p>
     </div>
   </div>
