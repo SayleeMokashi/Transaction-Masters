c8939d2614d1776532e22905a7e01d18bcf30738
SayleeMokashi <sayleev@gmail.com>
2025-02-17 23:05:04 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
   </a>
   </div>
 
-  <div class="nav-left d-flex align-items-center ms-auto">
+  <div class="nav-left d-flex ">
     <a class="navbar-brand me-5" href="#">
       <img
         class="BrandLogo1"
