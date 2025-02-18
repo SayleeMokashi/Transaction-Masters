8f1d6d8df7d76c47ee055871e9666985bb19b4db
SayleeMokashi <sayleev@gmail.com>
2025-02-18 23:17:35 +0530

    Update Pricing.js

--- components/Pricing.js
+++ components/Pricing.js
@@ -1,6 +1,6 @@
 function createPricingTable(title, price, index) {
   return `
-    <div class="pricing-table position-relative col-3 d-flex align-items-center justify-content-center text-center">
+    <div class="pricing-table position-relative col-3 col-sm-12 d-flex align-items-center justify-content-center text-center">
       <div class="inner-box overflow-hidden position-relative d-flex flex-column align-items-center justify-content-center" style="height: 100%;">
         <div class="title" style="font-size: 1.5rem; font-weight: bold;">${title}</div>
         <div class="price" style="font-size: 1.25rem; margin-top: 10px;">${price}</div>
