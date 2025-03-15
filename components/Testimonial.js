514f46c109edaa80ed136654aee290f0f4d37bfc
SayleeMokashi <sayleev@gmail.com>
2025-03-15 13:28:40 +0530

    Update Testimonial.js

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -14,8 +14,18 @@ function Testimonial() {
       <div id="testimonialCarousel" class="carousel slide w-100" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <div class="testimonial-item bg-white rounded p-4">
-              <p class="feed">As a real estate agent, I’ve worked with several transition coordinator companies, but this one stands out for its exceptional service...</p>
+            <div class="testimonial-item rounded p-4">
+              <p class="feed">
+                    As a real estate agent, I’ve worked with several transition coordinator companies, but this one stands out for its exceptional service. I rely on their expertise to help clients navigate the complexities of moving, downsizing, or relocating, and they've never disappointed. 
+                  </p>
+
+                  <p class="feed">
+                    Their team is incredibly organized, proactive, and always willing to go the extra mile to ensure everything runs smoothly. Communication is always top-notch, and they’re great at keeping clients informed and at ease throughout the transition. 
+                  </p>
+
+                  <p class="feed">
+                    It’s a true partnership—having them on board gives me confidence that my clients are in good hands and that their move will be as stress-free as possible. I highly recommend their services to any agent looking to elevate the client experience.
+                  </p>
               <div class="personal-detail d-flex flex-column mt-3">
                 <span class="per-name fw-bold">Jennifer Y</span>
                 <span class="per-role">Realtor, ExP</span>
@@ -23,8 +33,18 @@ function Testimonial() {
             </div>
           </div>
           <div class="carousel-item">
-            <div class="testimonial-item bg-white rounded p-4">
-              <p class="feed">In the past five years, I have been fortunate to have Saylee and Shraddha coordinate nearly 100 transactions...</p>
+            <div class="testimonial-item rounded p-4">
+              <p class="feed">
+                    In the past five years, I have been fortunate to have Saylee and Shraddha to coordinate nearly 100 transactions from contract to close. Their expertise makes the entire process seamless, allowing me to focus on the front end of my real estate business.
+                  </p>
+
+                  <p class="feed">
+                  Saylee and Shraddha’s communication is second to none, and they never miss a deadline. Their work is not only efficient and dependable but also consistently praised by clients. I’ve lost count of the number of reviews I’ve received that specifically highlight their professionalism and courtesy.
+                  </p>
+
+                  <p class="feed">
+                    As a full-time local Realtor, my name and reputation are of the utmost importance, and I am more than happy to vouch for the exceptional level of service these Transaction Masters provide with unwavering reliability. 
+                  </p>
               <div class="personal-detail d-flex flex-column mt-3">
                 <span class="per-name fw-bold">Brian L</span>
                 <span class="per-role">Realtor, EXP</span>
@@ -32,8 +52,18 @@ function Testimonial() {
             </div>
           </div>
           <div class="carousel-item">
-            <div class="testimonial-item bg-white rounded p-4">
-              <p class="feed">I've worked with Saylee and Shraddha for years on hundreds of transactions during my real estate career...</p>
+            <div class="testimonial-item rounded p-4">
+              <p class="feed">
+                    I've worked with  Saylee and Shraddha for years on hundreds of transactions during my real estate career and I couldn’t be more impressed with their level of service. They are hardworking, knowledgeable, and quick to respond to any agents, lenders, and clients alike. 
+                  </p>
+
+                  <p class="feed">
+                  They helped me keep track of all the moving parts of the process, ensuring nothing slipped through the cracks. Their extensive experience was evident in the way they keep things running smoothly. What stood out the most was their ability to communicate effectively with clients and always keeping me informed every step of the way.   
+                  </p>
+
+                  <p class="feed">
+                  I highly recommend Transaction Managers for anyone looking for experienced, reliable, and client-focused real estate transaction managers.    
+                  </p>
               <div class="personal-detail d-flex flex-column mt-3">
                 <span class="per-name fw-bold">Sherri J</span>
                 <span class="per-role">Woodward Realty</span>
