8c496949b57eeb489ad0d0e14f38da5e99a53744
SayleeMokashi <sayleev@gmail.com>
2025-03-15 14:31:37 +0530

    Update Herosection.js

--- components/Herosection.js
+++ components/Herosection.js
@@ -1,9 +1,7 @@
 function Herosection() {
   const Herosection = document.getElementById("Herosection");
   const Container = document.createElement("div");
-  const BG = document.createElement("div");
   Container.classList.add("container");
-  BG.classList.add("hero-bg");
   Container.innerHTML = `
   <div class="row row-cols-2 gx-5 gy-5">
           <div class="text-wrapper col-12 col-lg-7">
@@ -22,8 +20,6 @@ function Herosection() {
               <a class="button btn-theme-2" href="https://www.youtube.com/watch?v=3Sm3z8SEXMI" target="_blank">
               <i class="bi bi-play-circle me-2"></i> Watch Video
               </a>
-
-  
             </div>
           </div>
           <div
@@ -32,15 +28,7 @@ function Herosection() {
             <img class="text-wrapper" src="./Images/WhatsApp Image 2025-03-10 at 10.23.49 PM (7).jpeg" alt="" id="" />
           </div>
         </div>
-        
-  `;
-  BG.innerHTML = `
-        <!-- <img class="band left-bands" src="./Images/left-bands.svg"></img>
-        <img class="band right-bands" src="./Images/right-bands.svg"></img>
-       <div class="bg"></div>
-        <img class="purple-circle" src="./Images/Purple Circle.svg"></img> -->
   `;
   Herosection.appendChild(Container);
-  Herosection.appendChild(BG);
 }
 Herosection();
