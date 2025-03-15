8dba6c08ee8867e6507c0dc43ad8f00bbe1cb9bc
SayleeMokashi <sayleev@gmail.com>
2025-03-15 17:41:09 +0530

    Update Testimonial.js

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -20,7 +20,7 @@ function Testimonial() {
               <!-- <div class="client-logo">
                 <img src="./Images/Client-logo-01.svg" alt="" srcset="" />
               </div> -->
-              <div class="content mt-3">
+              <div class="content mt-2”>
                 <div class="feed-personalDetails">
                   <p class="feed">
                     As a real estate agent, I’ve worked with several transition coordinator companies, but this one stands out for its exceptional service. I rely on their expertise to help clients navigate the complexities of moving, downsizing, or relocating, and they've never disappointed. 
@@ -31,7 +31,7 @@ function Testimonial() {
                   <p class="feed">
                     It’s a true partnership—having them on board gives me confidence that my clients are in good hands and that their move will be as stress-free as possible. I highly recommend their services to any agent looking to elevate the client experience.
                   </p>
-                  <div class="personal-detail d-flex flex-column mt-3">
+                  <div class="personal-detail d-flex flex-column mt-2”>
                     <span class="per-name fw-bold"> Jennifer Y </span>
                     <span class="per-role"> Realtor, ExP  </span>
                   </div>
@@ -39,9 +39,9 @@ function Testimonial() {
               </div>
             </div>
           </div>
-          <div class="col mt-4">
+          <div class="col mt-2”>
             <div class="testimonial-item bg-white rounded">
-              <div class="content mt-3">
+              <div class="content mt-2”>
                 <div class="feed-personalDetails">
                   <p class="feed">
                     In the past five years, I have been fortunate to have Saylee and Shraddha to coordinate nearly 100 transactions from contract to close. Their expertise makes the entire process seamless, allowing me to focus on the front end of my real estate business.
@@ -52,15 +52,36 @@ function Testimonial() {
                   <p class="feed">
                     As a full-time local Realtor, my name and reputation are of the utmost importance, and I am more than happy to vouch for the exceptional level of service these Transaction Masters provide with unwavering reliability. 
                   </p>
-                  <div class="personal-detail d-flex flex-column mt-3">
+                  <div class="personal-detail d-flex flex-column mt-2”>
                     <span class="per-name fw-bold">Brian L</span>
                     <span class="per-role">Realtor, EXP</span>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="testimonial-item bg-white rounded mt-4">
-              <div class="content mt-3">
+            
+<div class="testimonial-item bg-white rounded mt-2”>
+              <div class="content mt-2”>
+                <div class="feed-personalDetails">
+                  <p class="feed">
+                    As a title Insurance processor, I work with many real estate professionals daily.  For over 5 years I have worked very closely with Saylee and Shraddha on countless transactions for several real estate agents.  Knowing that they are on the other side of my transactions brings a sense of security and ease to every deal. 
+                  </p>
+                  <p class="feed">
+                    Their combined knowledge of the industry and problem-solving abilities are second to none.  If you are looking for hardworking, organized, and professional transaction managers to assist in taking your business to the next level, Transaction Masters are who you need. 
+                  </p>
+                  <div class="personal-detail d-flex flex-column mt-2”>
+                    <span class="per-name fw-bold">Dana W.</span>
+                    <span class="per-role">First Centennial Title Agency</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+<div class="testimonial-item bg-white rounded mt-2”>
+              <div class="content mt-2”>
                 <div class="feed-personalDetails">
                   <p class="feed">
                     I've worked with Saylee and Shraddha for years on hundreds of transactions during my real estate career and I couldn’t be more impressed with their level of service. They are hardworking, knowledgeable, and quick to respond to any agents, lenders, and clients alike. 
@@ -71,7 +92,7 @@ function Testimonial() {
                   <p class="feed">
                     I highly recommend Transaction Managers for anyone looking for experienced, reliable, and client-focused real estate transaction managers.    
                   </p>
-                  <div class="personal-detail d-flex flex-column mt-3">
+                  <div class="personal-detail d-flex flex-column mt-2”>
                     <span class="per-name fw-bold">Sherri J</span>
                     <span class="per-role">Woodward Realty</span>
                   </div>
