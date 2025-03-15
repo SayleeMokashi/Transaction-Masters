6b368ae0edd2bcb2675646699b6a7e2c3a5b6816
SayleeMokashi <sayleev@gmail.com>
2025-03-15 14:37:53 +0530

    Update Herosection.js

--- components/Herosection.js
+++ components/Herosection.js
@@ -6,7 +6,7 @@ function Herosection() {
   <div class="row row-cols-2 gx-5 gy-5">
           <div class="text-wrapper col-12 col-lg-7">
             <div class="text-body">
-              <h1 class="lead mb-4 fw-bold">
+              <h1 class="lead mb-2 fw-bold">
                Our Story
               </h1>
         <p>Saylee and Shraddha's journey in real estate began with a shared understanding of the emotional complexities that come with buying or selling a home. Recognizing that the industry needed more than just transactional support, they founded a real estate transaction management company focused on streamlining processes while providing essential emotional guidance.</p>
