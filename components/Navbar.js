f9f63423364773c6bae0fdade1a162b7f1ef7e1f
SayleeMokashi <sayleev@gmail.com>
2025-03-22 21:08:04 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,14 +23,22 @@ function Navbar() {
       />
     </a>
   </div>
-  <div class="nav-right d-flex align-items-center ms-auto">
-    <div class="social-icons d-flex align-items-center me-4">
-      <a href="https://www.instagram.com/your_transactionmasters/" target="_blank" class="social-icon me-3">
+  <div class="nav-right d-flex flex-column align-items-end ms-auto">
+    <div class="contact-info d-flex align-items-center mb-2">
+      <a href="mailto:info@example.com" class="text-decoration-none me-3">
+        <i class="fas fa-envelope me-1"></i> info@example.com
+      </a>
+      <a href="tel:+1234567890" class="text-decoration-none">
+        <i class="fas fa-phone-alt me-1"></i> +123 456 7890
+      </a>
+    </div>
+    <div class="social-icons">
+      <a href="https://www.instagram.com/your_transactionmasters/" target="_blank" class="social-icon">
         <i class="fab fa-instagram fs-4"></i>
       </a>
     </div>
     <button
-      class="btn collapsed d-lg-none"
+      class="btn collapsed d-lg-none mt-2"
       type="button"
       data-bs-toggle="offcanvas"
       data-bs-target="#offcanvasRight"
@@ -44,3 +52,4 @@ function Navbar() {
   navbar.appendChild(Container);
 }
 Navbar();
+
